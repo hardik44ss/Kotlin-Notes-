99ff5f9480b314130c0c54b489d0b362d2f30761
Harbansh <142929609+hardik44ss@users.noreply.github.com>
2024-08-20 13:28:28 +0530

    Update 01_variable.kt

--- 01_variable.kt
+++ 01_variable.kt
@@ -1,7 +1,23 @@
 fun main() {
-    //Conditional statement
+    //Note : When you create a variable with the val keyword, the value cannot be changed/reassigned.
+    //When using var, you can change the value whenever you want:
+
     //var variableName = value
-	  var a ="Hardik " 
-	  var b ="Suthar"
-	  println(a+b)
+	var a ="Hardik " 
+	var b ="Suthar"
+	println(a+b)
+	var name: String = "John" // String
+	val birthyear: Int = 1975 // Int
+
+	/*var name
+	name ="Hardik " //This produce error */
+
+	println(name)
+	println(birthyear)
+
+	name = "John" // name is var
+	name = "Robert"
+	println("Hello " + name)
+	val pi = 3.14159265359
+	println(pi)
 }
