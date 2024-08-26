c84defad68e2ece3e1ff5678256e4e7d284b86f7
Harbansh <142929609+hardik44ss@users.noreply.github.com>
2024-08-26 22:40:43 +0530

    Update 08_Oops.kt

--- 08_Oops.kt
+++ 08_Oops.kt
@@ -3,18 +3,36 @@
 // OOP helps to keep the Kotlin code DRY "Don't Repeat Yourself", 
 // --DRY : reducing the repetation of code
 // OOP makes it possible to create full reusable applications with less code and shorter development time
-
 fun main() {
     val person1 = Person("John", 25)
+    val person2 = Person("Hardik", 12)
+    
     person1.displayInfo()
-
-    val person2 = Person("Hardik",12)
     person2.displayInfo()
+
+    val c1 = Car("Ford", "Mustang", 1969)
+    val c2 = Car("BMW", "X5", 1999)
+    val c3 = Car("Tesla", "Model S", 2020)
+    
+    c1.displayCarInfo()
+    c2.displayCarInfo()
+    c3.displayCarInfo()
+}
+
+// Define a class 'Car'
+class Car(var brand: String, var model: String, var year: Int) {
+    // Method to display car information
+    fun displayCarInfo() {
+        println("Brand: $brand")
+        println("Model: $model")
+        println("Year: $year")
+    }
 }
 
 // Define a class 'Person'
 class Person(val name: String, private val age: Int) {
-    fun displayInfo() { // Method to display information about the person
+    // Method to display information about the person
+    fun displayInfo() {
         println("Name: $name, Age: $age")
     }
 }
